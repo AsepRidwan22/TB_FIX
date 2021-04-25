099d68ee88748edd79dc7f387577b7f2812a7a04
AsepRidwan22 <aszeprydzone@gmail.com>
2021-04-24 18:06:24 -0700

    menambahkan autentikasi dan otorisai dengan middleware

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/main', function () {
+    return view('main');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/admin/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.home')->middleware('is_admin');
+
+Route::middleware('is_admin')->prefix('admin')->group(function(){
+    Route::get('/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.home');
+});
